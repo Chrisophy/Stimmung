cdb5ff4017736f0dccd7881dfe9bb12243fd5cfb
Chrisophy <darknobe@freenet.de>
2025-12-11 12:26:25 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,13 +3,10 @@ const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  // Hier weitere Assets hinzufügen, z.B. eigene CSS/JS-Dateien, wenn Sie diese nutzen
-  'https://cdn.tailwindcss.com',
-  'https://cdn.jsdelivr.net/npm/chart.js@4.4.2/dist/chart.umd.js',
-  // Beispiel Icons (passen Sie die Pfade an, wenn Sie sie anders nennen)
-  '/icons/icon-192x192.png', 
-  '/icons/icon-512x512.png'
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
 ];
+
 
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -28,11 +25,9 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache Hit - geben Sie die gecachte Ressource zurück
         if (response) {
           return response;
         }
-        // Cache Miss - versuchen Sie es mit dem Netzwerk
         return fetch(event.request);
       }
     )
@@ -46,7 +41,6 @@ self.addEventListener('activate', event => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-            // Lösche alte Caches
             return caches.delete(cacheName);
           }
         })
